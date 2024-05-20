cf349c5fc741f44db79e80f18c8754a7b7dd7874
Manuel <manuelloverso60@gmail.com>
2024-05-20 19:23:31 +0200

    edit show page

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        <h1>{{ $project->title }}</h1>
+    <div class="container py-4">
+        <div class="col-4">
+            <div class="card">
+                <div class="card-img">
+                    <img class="w-100" src="{{ $project->image }}" alt="{{ $project->image }}">
+                </div>
+                <div class="card-body">
+                    <h3>{{ $project->title }}</h3>
+                    <p><strong>Description: </strong>{{ $project->description }}</p>
+                    <p><strong>Technologies: </strong>{{ $project->technologies }}</p>
+                    <p><strong>Created: </strong>{{ $project->date }}</p>
+                    <a class="btn btn-dark" href="{{ route('admin.projects.edit', $project) }}">Edit</a>
+                    @include('partials.delete-modal')
+                    <a class="btn btn-dark" href="{{ route('admin.projects.index') }}">All Projects</a>
+
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -76,6 +76,8 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ url('admin') }}">{{ __('Dashboard') }}</a>
+                                    <a class="dropdown-item"
+                                        href="{{ route('admin.projects.index') }}">{{ __('Projects') }}</a>
                                     <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
